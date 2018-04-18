118a67fbaff115542fba63b1ed24af4f5d4f595b
Thomas <tbork17@student.aau.dk>
2018-04-18 11:21:23 +0100

    start skærm

---
+++ Game/New Unity Project/Assets/Scripts/ButtonWithSceneIndexAsAug.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonWithSceneIndexAsAug : MonoBehaviour {
+
+
+    public int SceneNum;
+    public int activeScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void buttonClicked() {
+
+        activeScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(SceneNum);
+    }
+}
---
+++ Game/New Unity Project/Assets/Scripts/ClickableObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickableObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnMouseDown(){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
