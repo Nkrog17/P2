1cfaba72eca07f6539af0cf968d21bf6450c5ab9
TheThob <tromed17@student.aa.dk>
2018-04-23 12:11:57 +0200

    text update

---
+++ Game/New Unity Project/Assets/Scripts/TextManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextManager : MonoBehaviour {
+
+    public Texts txt;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Scene 1: PoliceStation
+        if (SceneManager.GetActiveScene().buildIndex == 1) {
+            txt.setText1("Answer1");
+            txt.setText2("Answer2");
+            txt.setText3("Answer3");
+
+            txt.setQuestion("Question");
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                txt.turnOffObject();
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                txt.turnOnObject();
+            }
+        }
+        //Scene 2: GrenevsIntrance
+        else if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            txt.setText1("Answer1");
+            txt.setText2("Answer2");
+            txt.setText3("Answer3");
+
+            txt.setQuestion("Question");
+        }
+        //Scene 3
+        else if (SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            txt.setText1("Answer1");
+            txt.setText2("Answer2");
+            txt.setText3("Answer3");
+
+            txt.setQuestion("Question");
+        }
+        //Scene 4
+        else if (SceneManager.GetActiveScene().buildIndex == 4)
+        {
+            txt.setText1("Answer1");
+            txt.setText2("Answer2");
+            txt.setText3("Answer3");
+
+            txt.setQuestion("Question");
+        }
+    }
+}
---
+++ Game/New Unity Project/Assets/Scripts/Texts.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Texts : MonoBehaviour {
+
+    public Text one;
+    public Text two;
+    public Text three;
+
+    public Text question;
+
+    public GameObject vis;
+
+
+
+    public void setText1 (string aug){
+        one.text = aug;
+       }
+
+    public void setText2 (string aug)
+    {
+        two.text = aug;
+    }
+
+    public void setText3(string aug)
+    {
+        three.text = aug;
+    }
+
+    public void setQuestion(string aug)
+    {
+        question.text = aug;
+    }
+
+    public void turnOffObject()
+    {
+        vis.SetActive(false);
+
+    }
+
+    public void turnOnObject()
+    {
+        vis.SetActive(true);
+    }
+}
