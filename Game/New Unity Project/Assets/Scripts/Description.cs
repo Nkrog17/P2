edcdf1b61d5d8fa59362a3307c73a038aa34def2
Niels <nkrog17@student.aau.dk>
2018-05-09 11:31:11 +0200

    Working on stuff lol

--- Game/New Unity Project/Assets/Scripts/Description.cs
+++ Game/New Unity Project/Assets/Scripts/Description.cs
@@ -1,25 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Description : MonoBehaviour {
 
 	public string beskrivelse;
 	public GameObject descriptionbox;
 	bool showing = false;
-	public GameObject txt;
+	Text textbox;
+	int timer;
 
 	// Use this for initialization
 	void Start () {
 		descriptionbox.SetActive (false);
+		textbox = descriptionbox.GetComponentInChildren<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (showing) {
+			timer += 1;
+			if (timer >= 150) {
+				showing = false;
+				timer = 0;
+				descriptionbox.SetActive (false);
+			}
+		}
 	}
 
 	void OnMouseDown (){
-		descriptionbox.SetActive (true);
+		if (TextManager.conversationEnd) {
+			descriptionbox.SetActive (true);
+			textbox.text = beskrivelse;
+			if (showing) {
+				timer = 0;
+			}
+			showing = true;
+		}
 	}
 }
