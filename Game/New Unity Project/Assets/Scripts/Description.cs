445edb014d67c3b8bcecf05b17df8057d8d6d616
Niels <nkrog17@student.aau.dk>
2018-05-09 10:51:08 +0200

    Added Descriptions script

---
+++ Game/New Unity Project/Assets/Scripts/Description.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Description : MonoBehaviour {
+
+	public string beskrivelse;
+	public GameObject descriptionbox;
+	bool showing = false;
+	public GameObject txt;
+
+	// Use this for initialization
+	void Start () {
+		descriptionbox.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown (){
+		descriptionbox.SetActive (true);
+	}
+}
