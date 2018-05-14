aac63c9e4dd73501c2545a6f701ddebdd933865c
MaxwellSalmon <biuerboris@gmail.com>
2018-05-14 12:41:04 +0200

    Tilføjede dialog efter SQL

--- Game/New Unity Project/Assets/Scripts/TextManager.cs
+++ Game/New Unity Project/Assets/Scripts/TextManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -114,22 +114,24 @@ public class TextManager : MonoBehaviour {
             conversationEnd = true;
         }
         //Scene 4: Værelse med lys
-		else if (SceneManager.GetActiveScene().buildIndex == 4 && kam.kamClicked)
+        else if (SceneManager.GetActiveScene().buildIndex == 4)
         {
-			if (An == 1) {
-				conversationEnd = false;
-				txt.setQuestion ("“Den kam genkender jeg ikke!”");
-				txt.setText1 ("");
-				txt.setText2 ("“Der er mørke hår på den.”");
-				txt.setText3 ("");
-			}
-			if (An == 11) {
-				txt.setQuestion ("“Karmen har lyst hår! Det er ikke hendes kam!”");
-				txt.setText1 ("");
-				txt.setText2 ("“Jeg tager den med og undersøger den.”");
-				txt.setText3 ("");
-				conversationEnd = true;
-			}
+            if (An == 1){
+                conversationEnd = false;
+                txt.turnOnObject();
+                txt.setQuestion("“Den kam kenkender jeg ikke... ”");
+                txt.setText1("");
+                txt.setText2("“Der er mørke hår på den.”");
+                txt.setText3("");
+            }
+            if (An == 11){
+                txt.setQuestion("“Karmen har lyst hår! Det er ikke hendes kam!”");
+                txt.setText1("");
+                txt.setText2("“Jeg tager dem med på laboratoriet og undersøger den.”");
+                txt.setText3("");
+                conversationEnd = true;
+            }
+            
         }
 
         //Scene 5: Kælder
@@ -196,7 +198,7 @@ public class TextManager : MonoBehaviour {
         //scene 8: Efter DNA-spil
         else if (SceneManager.GetActiveScene().buildIndex == 8){
             if (An == 1){
-                conversationEnd = false;
+                conversatonEnd = false;
                 txt.turnOnObject();
                 txt.setQuestion("“Pefekt! Du er færdig med opgaven. Jeg undersøger resultaterne... Det ser ud til, at kammen tilhører en Ulrik Olsen.”");
                 txt.setText1("“Har vi mere information om ham?”");
@@ -349,8 +351,38 @@ public class TextManager : MonoBehaviour {
             }
         }
 
-        //Scene 12: tank
-        else if (SceneManager.GetActiveScene().buildIndex == 12)
+        //scene 12: efter sql
+        else if (SceneManager.GetActiveScene().buildIndex == 12){
+            if (An == 1){
+                txt.turnOnObject();
+                conversationEnd = false;
+                txt.setQuestion("“Perfekt! Du har fundet ham! Jeg ser, om jeg kan finde bilen.”");
+                txt.setText1("“Skynd dig!”");
+                txt.setText2("");
+                txt.setText3("“Hvordan gør du?”");
+            }
+            if (An == 2){
+                txt.setQuestion("“Rolig nu. Det kræver tålmodighed. Jeg skal nok finde ham.”");
+                txt.setText1("");
+                txt.setText2("“Okay!”");
+                txt.setText3("");
+            }
+            if (An == 101){
+                txt.setQuestion("“Jeg har adgang til adskillige overvågningskameraer i landet. Jeg ser, om bilen er set på et af dem.”");
+                txt.setText1("“Sejt!”");
+                txt.setText2("");
+                txt.setText3("“Okay!”");
+            }
+            if (An == 102 || An == 201 || An == 12){
+                txt.setQuestion("“Jeg har fundet noget! Hans bil er blevet set på en tankstation ikke langt herfra.”");
+                txt.setText1("");
+                txt.setText2("“Jeg tager traks afsted!”");
+                txt.setText3("");
+                conversationEnd = true;
+            }
+        }
+        //Scene 13: tank
+        else if (SceneManager.GetActiveScene().buildIndex == 13)
         {
             if (An == 1)
             {
@@ -411,7 +443,7 @@ public class TextManager : MonoBehaviour {
             {
                 txt.setQuestion("“Han kørte for X minutter siden og havde ikke travlt. Han kørte ved 60 kilometer i timen, som man må heromkring.”");
                 txt.setText1("");
-                txt.setText2("“Du kommer med mig! Jeg må finde ud af, hvor han kan være kørt hen”");
+                txt.setText2("“Du kommer med mig! Jeg må regne ud, hvor han kan være kørt hen”");
                 txt.setText3("");
             }
 
@@ -422,8 +454,8 @@ public class TextManager : MonoBehaviour {
             }
         }
 
-        //Scene 14: Lade
-        else if (SceneManager.GetActiveScene().buildIndex == 14)
+        //Scene 15: Lade
+        else if (SceneManager.GetActiveScene().buildIndex == 15)
         {
             if (An == 1)
             {
@@ -479,8 +511,8 @@ public class TextManager : MonoBehaviour {
             }
         }
 
-        // Scene slut
-        if (SceneManager.GetActiveScene().buildIndex == 15)
+        // Scene slut 16
+        if (SceneManager.GetActiveScene().buildIndex == 16)
         {
             if (An == 1)//Møller
             {
