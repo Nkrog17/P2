867daad368fb598734cc2152b362b49b9361dd82
Thomas <tbork17@student.aau.dk>
2018-05-14 10:33:41 +0100

    DNA Puzzle

---
+++ Game/New Unity Project/Assets/Scripts/uWonText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uWonText : MonoBehaviour {
+
+
+    public GameObject distanceText;
+
+    public bool uWon = false;
+
+
+    public void WinCondition()
+    {
+
+        if (Behavoir.numberOfCollidersCorrect >= 20)
+        {
+
+            if (!uWon)
+            {
+                Debug.Log("U WON !!!");
+                UWonTexT();
+                uWon = true;
+            }
+
+        }
+
+    }
+
+    public void UWonTexT()
+    {
+        distanceText.SetActive(true);
+
+    }
+
+    public void whenClicked()
+    {
+            
+            Debug.Log("now");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
