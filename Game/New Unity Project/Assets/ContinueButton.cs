0b66b2ad349a3577ed75cf23c0720b19d876e6a3
Mathias <mjensa17@student.aau.dk>
2018-05-14 11:40:52 +0200

    math puzzle

---
+++ Game/New Unity Project/Assets/ContinueButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContinueButton : MonoBehaviour {
+
+    public GameObject click;
+    //public GameObject house;
+    public GameObject button;
+    public Slider radiusSlider;
+    public Slider speedSlider;
+    public Slider timeSlider;
+
+	// Use this for initialization
+	void Start () {
+        button.SetActive(false);
+	}
+
+    // Update is called once per frame
+    void Update() {
+/*
+        if (radiusSlider.value > 24.77 && radiusSlider.value < 25.27)
+            if (speedSlider.value > 58 && speedSlider.value < 62)
+                if (timeSlider.value > 4.5 && timeSlider.value < 5.5)
+                   if (click.activeSelf == true) {
+                    button.SetActive(true);
+                }
+                else
+                {
+                    button.SetActive(false);
+                }*/
+	}
+}
---
+++ Game/New Unity Project/Assets/WhenClicked.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WhenClicked : MonoBehaviour {
+
+    public int num;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void ButtonClick ()
+    {
+        SceneManager.LoadScene(num);
+        
+    }
+}
