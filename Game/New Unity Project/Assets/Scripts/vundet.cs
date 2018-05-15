b327f7feac3a1cc036a07b67903e0e860c0f38b0
MaxwellSalmon <biuerboris@gmail.com>
2018-05-15 14:14:18 +0200

    Slut-skærm tilføjet

---
+++ Game/New Unity Project/Assets/Scripts/vundet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vundet : MonoBehaviour {
+
+	public GameObject txt;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TextManager.conversationEnd) {
+			txt.SetActive (true);
+		} else {
+			txt.SetActive (false);
+		}
+	}
+}
