5677809efb321d20fc4b846c2be9ce8f7c288293
TheThob <tromed17@student.aa.dk>
2018-05-15 16:24:20 +0200

    hot fix 1577

---
+++ Game/New Unity Project/Assets/click.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class click : MonoBehaviour {
+
+    public AudioClip clip;
+
+    public AudioSource source;
+
+
+	// Use this for initialization
+	void Awake () {
+        source.clip = clip;
+	}
+	
+	// Update is called once per frame
+	public void playClick()
+    {
+        source.Play();
+    }
+}
